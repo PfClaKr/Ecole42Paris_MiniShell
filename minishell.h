118d2cdc57ce5242092bc38d040ec481f006cde3
ychun <ychun@student.42.fr>
2023-01-20 17:16:46 +0100

    Start to minishell

---
+++ minishell.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychun <ychun@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 18:13:42 by ychun             #+#    #+#             */
+/*   Updated: 2023/01/16 18:31:07 by ychun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include "libft/libft.h"
+
+# define FALSE 0
+# define TRUE 1
+
+# define ERROR -1
+# define SUCCESS 1
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+#endif
